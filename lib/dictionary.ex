75054567596484ad3aa4442f0106d88823e11790
Fern <frlimaa@gmail.com>
2023-12-29 22:06:45 -0600

    more functional

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,10 +1,12 @@
 defmodule Dictionary do
   def word_list do
-    words = File.read!('assets/words.txt')
-    String.split(words, ~r/\n/, trim: true)
+    "assets/words.txt"
+    |> File.read!
+    |> String.split(~r/\n/, trim: true)
   end
 
   def random_word do
-    word_list() |> Enum.random
+    word_list()
+    |> Enum.random
   end
 end
